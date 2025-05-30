e923deb46e7b6ec0ce6bf7e9a4800b0b6f98fa6b
mishobo <husseinabdallah287@gmail.com>
2025-05-30 11:32:43 +0300

    order method execution by method name

---
+++ src/test/java/com/mishobo/test_driven_development/MethodOrderedByNameTest.java
@@ -0,0 +1,43 @@
+package com.mishobo.test_driven_development;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.DisplayName.class)
+public class MethodOrderedByNameTest {
+
+    @Test
+    void testA(){
+        System.out.println("testA");
+    }
+
+    @Test
+    void testD(){
+        System.out.println("testD");
+    }
+
+    @Test
+    void testE(){
+        System.out.println("testE");
+    }
+
+
+    @Test
+    void testC(){
+        System.out.println("testC");
+    }
+
+    @Test
+    void testB(){
+        System.out.println("testB");
+    }
+
+    @Test
+    void testF(){
+        System.out.println("testF");
+    }
+
+
+}
+
